af235e53a4407a863093efec8b0f9f8526591636
Antony Baasandorj <antony.baasan@gmail.com>
2017-06-30 00:27:26 -0400

    add react-router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,22 @@
 import 'babel-polyfill';
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
 );
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default Root;
